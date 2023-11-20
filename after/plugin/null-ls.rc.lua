0ee2608788b9ee4a9d61d5c1ad9ec4d6d00b8be1
rolled-potatoes <keep4404@gmail.com>
2023-11-20 22:42:46 +0900

    feat: prettier eslint 적용

--- after/plugin/null-ls.rc.lua
+++ after/plugin/null-ls.rc.lua
@@ -3,6 +3,11 @@ local status, null_ls = pcall(require, 'null-ls')
 if (not status) then return end
 
 null_ls.setup({
+      sources = {
+        null_ls.builtins.diagnostics.eslint, -- eslint or eslint_d
+        null_ls.builtins.code_actions.eslint, -- eslint or eslint_d
+        null_ls.builtins.formatting.prettier -- prettier, eslint, eslint_d, or prettierd
+    },
   on_attach = function(client, bufnr)
   end,
 })
