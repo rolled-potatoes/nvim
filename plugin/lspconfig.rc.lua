1b74c89d11b80226455410cf898741d58d70bb1e
harleyPark <harley.park@goorm.io>
2024-07-30 12:19:58 +0900

    Change lsp plugin pcall
    추론 서버가 없는 경우 경고문구가 계속 띄워져서 변경함

--- plugin/lspconfig.rc.lua
+++ plugin/lspconfig.rc.lua
@@ -21,32 +21,37 @@ nvim_lsp.tsserver.setup {
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-nvim_lsp.cssls.setup{
-  on_attach = on_attach,
-  cmd={ "vscode-css-language-server", "--stdio" },
-  filetypes  = { "css", "scss", "less" },
-  capabilities = capabilities,
-}
+pcall(function()
+  nvim_lsp.cssmodules_ls.setup{
+    cmd = { "cssmodules-language-server" },
+    filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+  }
+end)
 
-nvim_lsp.cssmodules_ls.setup{
-  cmd = { "cssmodules-language-server" },
-  filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-}
+pcall((function()
+  nvim_lsp.cssls.setup {
+    on_attach = on_attach,
+    cmd = { "vscode-css-language-server", "--stdio" },
+    filetypes = { "css", "scss", "less" },
+    capabilities = capabilities,
+  }
+end))
 
-nvim_lsp.lua_ls.setup {
-  on_attach = on_attach,
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { 'vim' }
+pcall(function()
+  nvim_lsp.lua_ls.setup {
+    on_attach = on_attach,
+    settings = {
+      Lua = {
+        diagnostics = {
+          globals = { 'vim' }
+        }
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true)
       }
-    },
-    workspace = {
-      library = vim.api.nvim_get_runtime_file("", true)
     }
   }
-}
+end)
+pcall(nvim_lsp.docker_compose_language_service)
+pcall(nvim_lsp.tailwindcss.setup);
 
-nvim_lsp.docker_compose_language_service.setup{}
-
-nvim_lsp.tailwindcss.setup{}
