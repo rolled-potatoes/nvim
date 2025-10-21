0c668145410cc7e4f319ab91a44c9237add7fb0b
harley.park <harley.park@goorm.io>
2025-10-21 03:44:08 +0000

    Add base vim option

---
+++ lua/config/option.lua
@@ -0,0 +1,21 @@
+-- vim 기본 옵션 설정 
+vim.opt.encoding  	  =	'utf-8'
+vim.opt.fileencoding	=	'utf-8'
+vim.wo.number		      =	true
+vim.opt.title		      =	true
+vim.opt.hlsearch	    =	true
+vim.opt.shell		      =	'fish'
+
+vim.opt.expandtab     = true
+vim.opt.smartindent   = true
+vim.opt.shiftwidth    = 2
+vim.opt.tabstop       = 2
+vim.opt.softtabstop   = 2
+
+vim.opt.wrap          = true
+vim.opt.smarttab      = true
+vim.opt.linebreak     = true
+vim.opt.showbreak     = "↳ "
+vim.opt.breakindent   = true
+
+vim.opt.swapfile      = false
