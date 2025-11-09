21a14a80c9166653c9474e95885e4a2f8cdf285e
rolled-potatoes <keep4404@gmail.com>
2025-11-09 14:36:10 +0900

    lsp 설정

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,41 +1,63 @@
 -- lua/plugins/lsp.lua
 return {
-  -- 1. LSP 핵심 설정 (lspconfig)
-  {
-    'neovim/nvim-lspconfig',
-    -- 플러그인이 로드될 때 실행할 설정 함수
-    config = function()
-      -- === 필수 모듈 로드 ===
-      local lspconfig = require('lspconfig')
-      local mason_lspconfig = require('mason-lspconfig')
-      -- === 3. Mason 설정 (언어 서버 설치) ===
-      -- Node.js 풀스택 개발을 위한 기본 언어 서버 목록
-      local servers = {
-        "ts_ls",   -- TypeScript/JavaScript
-        "eslint",     -- ESLint (Linter)
-        "jsonls",     -- JSON
-        "cssls",      -- CSS
-        "html",       -- HTML
-        "dockerls",   -- Dockerfile
-        "marksman",   -- Markdown
-        "bashls",     -- Shell 스크립트
-        "tailwindcss", 
-        "stylua"
-      }
+	-- 1. LSP 핵심 설정 (lspconfig)
+	{
+		"neovim/nvim-lspconfig",
+		-- 플러그인이 로드될 때 실행할 설정 함수
+		config = function()
+			-- === 필수 모듈 로드 ===
+			local lspconfig = require("lspconfig")
+			local mason_lspconfig = require("mason-lspconfig")
+			-- === 3. Mason 설정 (언어 서버 설치) ===
+			-- Node.js 풀스택 개발을 위한 기본 언어 서버 목록
+			local servers = {
+				"ts_ls", -- TypeScript/JavaScript
+				"eslint", -- ESLint (Linter)
+				"jsonls", -- JSON
+				"cssls", -- CSS
+				"html", -- HTML
+				"dockerls", -- Dockerfile
+				"marksman", -- Markdown
+				"bashls", -- Shell 스크립트
+				"tailwindcss",
+				"stylua",
+			}
 
-      require('mason').setup()
-      mason_lspconfig.setup({
-        ensure_installed = servers, -- 이 서버들이 자동으로 설치되도록 보장
-        automatic_installation = true, -- 서버가 없을 경우 자동 설치
-        automatic_enable = true
-      })
-    end,
+			require("mason").setup()
+			mason_lspconfig.setup({
+				ensure_installed = servers, -- 이 서버들이 자동으로 설치되도록 보장
+				automatic_installation = true, -- 서버가 없을 경우 자동 설치
+				automatic_enable = true,
+			})
+			local on_attach = function(client, bufnr)
+				-- 도움말, 정의 추적, 코드 액션 등 LSP 핵심 기능 키매핑
+				local opts = { buffer = bufnr, noremap = true, silent = true }
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+				vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+				vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 
-    -- === 의존성 플러그인 목록 ===
-    dependencies = {
-      -- 언어 서버 자동 설치 및 관리
-      'mason-org/mason.nvim',
-      'mason-org/mason-lspconfig.nvim',
-    },
-  },
+				-- 진단(Diagnostics) 관련 키매핑
+				vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+				vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+				vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
+			end
+
+			for _, server_name in ipairs(servers) do
+				vim.lsp.config(server_name, {
+					on_attach = on_attach,
+				})
+			end
+		end,
+
+		-- === 의존성 플러그인 목록 ===
+		dependencies = {
+			-- 언어 서버 자동 설치 및 관리
+			"mason-org/mason.nvim",
+			"mason-org/mason-lspconfig.nvim",
+		},
+	},
 }
--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,39 +1,42 @@
-local function on_attach(bufnr)
-  local api = require "nvim-tree.api"
-
-  local function opts(desc)
-    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-  end
-
-  -- default mappings
-  api.config.mappings.default_on_attach(bufnr)
-  vim.keymap.set('n','sv',    api.node.open.vertical,   opts('세로 분할'))
-  vim.keymap.set('n','ss',    api.node.open.horizontal, opts('가로 분할'))
-end
-
 -- keymap
-  -- 토글 
-vim.keymap.set('n','<C-n>t',':NvimTreeToggle <CR>')
-  -- 현재 열린 파일로 트리 검색
-vim.keymap.set('n','<C-n>f',':NvimTreeFindFile <CR>')
+-- 토글
+-- vim.keymap.set('n','<C-n>t',':NvimTreeToggle <CR>')
+-- 현재 열린 파일로 트리 검색
+-- vim.keymap.set('n','<C-n>f',':NvimTreeFindFile <CR>')
 
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 return {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
-  lazy = false,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  on_attach = on_attach,
-  config = function()
-    require("nvim-tree").setup {
-      git = {
-        ignore = true
-      }
-    }
-  end,
+	"nvim-tree/nvim-tree.lua",
+	version = "*",
+	lazy = false,
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		local function on_attach(bufnr)
+			local api = require("nvim-tree.api")
+
+			local function opts(desc)
+				return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+			end
+
+			-- default mappings
+			api.config.mappings.default_on_attach(bufnr)
+			vim.keymap.set("n", "sv", api.node.open.vertical, opts("세로 분할"))
+			vim.keymap.set("n", "ss", api.node.open.horizontal, opts("가로 분할"))
+		end
+		require("nvim-tree").setup({
+
+			on_attach = on_attach,
+			git = {
+				ignore = true,
+			},
+		})
+		vim.keymap.set("n", "<C-n>t", ":NvimTreeToggle <CR>")
+		-- 현재 열린 파일로 트리 검색
+		vim.keymap.set("n", "<C-n>f", ":NvimTreeFindFile <CR>")
+	end,
 }
