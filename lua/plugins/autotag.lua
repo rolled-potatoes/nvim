b80aebfbd3d216186eae2252242763b76a728fed
rolled-potatoes <keep4404@gmail.com>
2025-11-17 21:10:15 +0900

    set auto tag

---
+++ lua/plugins/autotag.lua
@@ -0,0 +1,10 @@
+
+return {
+    'windwp/nvim-ts-autotag',
+    event = "InsertEnter",
+    config = function ()
+      local autotag = require('nvim-ts-autotag');
+
+      autotag.setup{}
+    end
+}
--- lua/plugins/tresitter.lua
+++ lua/plugins/tresitter.lua
@@ -2,4 +2,13 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    config = function()
+      local ts = require('nvim-treesitter.configs')
+      ts.setup{
+        ensure_installed = {
+          "lua", "vim", "markdown", "tsx", "css","html", "javascript",'typescript', 'json','scss',
+        },
+        auto_install = true, 
+      }
+    end
 }
