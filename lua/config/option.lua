32ec64803c2e22be0e00149138378bb49dc0420c
rolled-potatoes <keep4404@gmail.com>
2026-02-10 22:31:59 +0900

    feat: theme transparent 설정

--- lua/config/option.lua
+++ lua/config/option.lua
@@ -21,8 +21,6 @@ vim.opt.breakindent   = true
 vim.opt.swapfile      = false
 vim.opt.clipboard     = "unnamedplus"
 
-vim.cmd[[colorscheme tokyonight]]
-
 -- 라인 넘버 색상 설정 (진한 흰색)
 vim.api.nvim_set_hl(0, 'LineNr', { fg = '#ffffff', bold = true })
 vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = '#ffffff', bold = true })
